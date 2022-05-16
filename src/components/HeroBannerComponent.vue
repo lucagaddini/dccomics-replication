b440a340bfd432941f31d9c328b83917e4e4f525
lucagad <lucagaddini@icloud.com>
2022-05-16 14:04:59 +0200

    {Vue} Added: HeroBannerComponent

---
+++ src/components/HeroBannerComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="jumbotron">
+    <img src="https://guidatv.sky.it/uuid/3e779445-c434-4d5d-9ef3-71ac4799a87e/16-9?md5ChecksumParam=b86cd899991a006dfe75d9aac8990f16&tr=n-scene_128" alt="">
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HeroBannerComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import '../assets/style/_mixins.scss';
+@import '../assets/style/_vars.scss';
+@import '../assets/style/_general.scss';
+
+.jumbotron{
+  height: 400px;
+  @include flex-cnt();
+  overflow: hidden;
+}
+
+
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <main>
-    <h3>-- Content goes here --</h3>
+    <div class="container">
+      <div class="title_box">Current Series</div>
+    </div>
   </main>
 </template>
 
@@ -24,6 +26,23 @@ export default {
     width: 100%;
     background-color: $main-background-color;
     color: white;
-    
+    position: relative;
+
+    .title_box{
+      font-size: 25px;
+      position: absolute;
+      top: -25px;
+      left: 230px;
+      margin: 0;
+      padding-top: 10px;
+      padding-right: 25px;
+      padding-bottom: 8px;
+      padding-left: 25px;
+      line-height: 0.9;
+      color: #fff;
+      background-color: $primary-color;
+      text-transform: uppercase;
+      font-weight: 600;
+    }
   }
 </style>
