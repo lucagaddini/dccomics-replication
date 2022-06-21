e8db451e067ca227639fcfeafb72f21ef33c87e9
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-06-21 15:31:27 +0200

    Added: PortfolioHeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <PortfolioHeaderComponent />
     <HeaderComponent />
     <HeroBannerComponent />
     <MainComponent />
@@ -14,9 +15,10 @@ import MainComponent from './components/MainComponent.vue'
 import PreFooterComponent from './components/PreFooterComponent.vue'
 import FooterComponent from './components/FooterComponent.vue'
 import HeroBannerComponent from './components/HeroBannerComponent.vue'
+import PortfolioHeaderComponent from './components/PortfolioHeaderComponent.vue'
 
 export default {
-  components: { HeaderComponent, MainComponent, PreFooterComponent, FooterComponent, HeroBannerComponent },
+  components: { HeaderComponent, MainComponent, PreFooterComponent, FooterComponent, HeroBannerComponent, PortfolioHeaderComponent },
   
 }
 </script>
